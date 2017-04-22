9c9fe77f95ff1a98aaf630f8a9a5fca7823c527a
janikibichi <janikibichi@gmail.com>
2017-04-22 14:10:05 +0300

    env variables

--- hellofrank.rb
+++ hellofrank.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
-require 'AfricasTalkingGateway'
+#require 'AfricasTalkingGateway'
+require './AfricasTalkingGateway.rb'
 require 'dotenv'
 Dotenv.load #If more that one .env then Dotenv.load('one.env', 'two.env')
 
@@ -8,5 +9,17 @@ get '/frank-says' do
 
 	'put this in your pipe & smoke it'
 	
+end
+
+#Send the SMS
+get '/airtime-manenos' do
+	#pull env variable
+	username = ENV['API_LIVE_USERNAME']
+	apikey = ENV['API_LIVE_KEY']
+	#create instance of Gateway class
+	gateway = AfricasTalkingGateway.new(username, apikey)
+	#Send SMS
+	result = gateway.sendMessage('+254708415904,+254701556803', 'Hello airtime, where are you?')
+	puts result
 end
 
