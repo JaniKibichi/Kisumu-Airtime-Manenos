50501bb07cceae5e7fec154f6b235672f9b971cd
janikibichi <janikibichi@gmail.com>
2017-04-22 16:24:33 +0300

    airtime manenos POST /

--- hellofrank.rb
+++ hellofrank.rb
@@ -8,7 +8,6 @@ Dotenv.load #If more that one .env then Dotenv.load('one.env', 'two.env')
 get '/frank-says' do 
 
 	'put this in your pipe & smoke it'
-	
 end
 
 #Send the SMS
@@ -24,8 +23,95 @@ get '/airtime-manenos' do
 end
 
 post '/airtime-manenos' do
+	#pull env variable
+	username = ENV['API_LIVE_USERNAME']
+	apikey = ENV['API_LIVE_KEY']
+
 	#1. Get all the POST Parameters from AT
+		#a. short code (http://docs.africastalking.com/sms/callback)
+		@from = params[:from]
+		@to = params[:to]
+		@date = params[:date]
+		@text = params[:text]
+		@id = params[:id]
+		@linkId = params[:linkId]
+
+		#b. Mpesa (http://docs.africastalking.com/payments/notification)
+		@category = params[:category]
+		@providerRefId = params[:providerRefId]
+		@productName = params[:productName]
+		@sourceType = params[:sourceType]	
+		@source = params[:source]
+		@destination = params[:destination]
+		@value = params[:value]
+		@status = params[:status]
+		@description = params[:description]
+		@requestMetadata = params[:requestMetadata]
+		@providerMetadata = params[:providerMetadata]
+		@transactionDate = params[:transactionDate]				
+	
+		#c. airtime (http://docs.africastalking.com/airtime/callback)
+		@status = params[:status]
+		@requestId = params[:requestId]
+
 	#2. For the text - via short code - explode and take the last value
-	#3. Do Mpesa-Checkout
-	#4. On Success - send airtime
+		#2.a. explode text & Do Mpesa-Checkout
+		if !@text.nil?	
+			@airtimeRequest = @text.split(' ').last.to_i
+			#2.a.1. Mpesa Checkout
+			sendCheckout(@airtimeRequest.to_i, @from)
+		end		
+	#3. On Success - send airtime
+		#3.a. if the providerRefId exits (on success) and category is MobileCheckout
+		if !@providerRefId.nil?	&& @category == "MobileCheckout"
+			@airtimeValue = @value.split(' ').last.to_i
+			#3.a.1 send airtime
+			sendAirtime(@airtimeValue.to_i, @source)			
+		end
+
+	#Put all the params to the console
+	if params[:linkId]
+		puts params + "Sent request for airtime++++++++++++++++++++++++++++"
+	end
+
+	if params[:category]
+		puts params + "Just paid for airtime++++++++++++++++++++++++++++"
+	end
+
+	if params[:requestId]
+		puts params + "Just received airtime++++++++++++++++++++++++++++"
+	end	
+	
+
+	#methods
+	def sendCheckout(amount,@from)
+		#define params
+		productName  = "Nerd Payments"
+		currencyCode = "KES"
+		metadata     = {"product"   : "airtime","client" : @phoneNumber}
+		gateway = AfricasTalkingGateway.new(username, apikey)
+		begin
+		    # Initiate the checkout. If successful, you will get back a transactionId
+		    transactionId = gateway.initiateMobilePaymentCheckout(productName, @phoneNumber,  currencyCode, amount, metadata)
+		    puts transactionId
+		    
+		rescue Exception => ex
+		    puts "Encountered an error with Mpesa Checkout: " + ex.message
+		end
+	end
+
+	#methods
+	def sendAirtime(amount, @source)
+		#define params
+		recipients = Array.new
+		recipients[0] = {"phoneNumber" => @phoneNumber, "amount" => amount}
+		gateway = AfricasTalkingGateway.new(username, apikey)
+		begin
+			#send Airtime
+			results = gateway.sendAirtime(recipients)
+			puts results
+		rescue AfricasTalkingGatewayException => ex
+		  puts 'Encountered an error with airtime sending: ' + ex.message
+		end
+	end	
 end 
